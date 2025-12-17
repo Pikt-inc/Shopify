956ed5dc2770147387897fbc4dfa1c18858ee08f
Patten Whiting <patten.whiting@gmail.com>
2025-12-16 21:59:58 -0600

    Version bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='shopify_sdk',
-    version='0.1.2',
+    version='0.1.3',
     author='Pk whiting',
     author_email='patten.whiting@gmail.com',
     description='An updated OO Sdk for Shopify GQL',
---
+++ shopify_sdk/common/store/__init__.py
@@ -0,0 +1,7 @@
+from .inventory import update_inventory
+from .locations import get_locations
+
+__all__ = [
+    "update_inventory",
+    "get_locations",
+]
