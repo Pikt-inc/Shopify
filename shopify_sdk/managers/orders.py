044c22872e9650fb1001cff2b92751f1a84df3bd
Patten Whiting <patten.whiting@gmail.com>
2025-12-29 11:33:24 -0600

    reformat

--- shopify_sdk/managers/orders.py
+++ shopify_sdk/managers/orders.py
@@ -4,8 +4,12 @@ import datetime
 if TYPE_CHECKING:
     from shopify_sdk.gql.core.types.objects import Order
     from shopify_sdk.gql.core.types.base import ID
-    from shopify_sdk.gql.core.types.enums import OrderDisplayFulfillmentStatus, OrderDisplayFinancialStatus
+    from shopify_sdk.gql.core.types.enums import (
+        OrderDisplayFulfillmentStatus,
+        OrderDisplayFinancialStatus,
+    )
     from shopify_sdk.gql.core.types.connections import OrderConnection
+
 
 class Time:
     def _utc_now(self) -> datetime.datetime:
@@ -23,7 +27,9 @@ class Time:
     def _quote(self, value: datetime.datetime) -> str:
         return f"'{self._format(value)}'"
 
-    def _between_constraints(self, start: datetime.datetime, end: datetime.datetime) -> str:
+    def _between_constraints(
+        self, start: datetime.datetime, end: datetime.datetime
+    ) -> str:
         return f">={self._quote(start)} created_at:<={self._quote(end)}"
 
     @property
@@ -31,37 +37,38 @@ class Time:
         now = self._utc_now()
         start_of_day = now.replace(hour=0, minute=0, second=0, microsecond=0)
         return self._between_constraints(start_of_day, now)
-    
+
     @property
     def THIS_WEEK(self) -> str:
         now = self._utc_now()
         start_of_week = now - datetime.timedelta(days=now.weekday())
         start_of_week = start_of_week.replace(hour=0, minute=0, second=0, microsecond=0)
         return self._between_constraints(start_of_week, now)
-    
+
     @property
     def THIS_MONTH(self) -> str:
         now = self._utc_now()
         start_of_month = now.replace(day=1, hour=0, minute=0, second=0, microsecond=0)
         return self._between_constraints(start_of_month, now)
-    
+
     @property
     def LAST_30_DAYS(self) -> str:
         now = self._utc_now()
         past_30_days = now - datetime.timedelta(days=30)
         return self._between_constraints(past_30_days, now)
-    
+
     @property
     def LAST_90_DAYS(self) -> str:
         now = self._utc_now()
         past_90_days = now - datetime.timedelta(days=90)
         return self._between_constraints(past_90_days, now)
-    
+
     @property
     def LAST_180_DAYS(self) -> str:
         now = self._utc_now()
         past_180_days = now - datetime.timedelta(days=180)
         return self._between_constraints(past_180_days, now)
+
 
 class OrderManager:
     Time = Time()
@@ -94,12 +101,8 @@ class OrderManager:
 
     def query(
         self,
-        fulfillment_status: Optional[
-            "OrderDisplayFulfillmentStatus"
-        ] = None,
-        financial_status: Optional[
-            "OrderDisplayFinancialStatus"
-        ] = None,
+        fulfillment_status: Optional["OrderDisplayFulfillmentStatus"] = None,
+        financial_status: Optional["OrderDisplayFinancialStatus"] = None,
         time: Optional[str] = None,
     ) -> "OrderConnection":
         from shopify_sdk.gql.queries import orders
@@ -112,9 +115,7 @@ class OrderManager:
                     f"fulfillment_status:{fulfillment_status.value.lower()}"
                 )
             if financial_status:
-                query_parts.append(
-                    f"financial_status:{financial_status.value.lower()}"
-                )
+                query_parts.append(f"financial_status:{financial_status.value.lower()}")
             if time:
                 query_parts.append(f"created_at:{time}")
             query_string = " ".join(query_parts)
