07d7671072696dee75148f52178b730644b82577
Patten Whiting <patten.whiting@gmail.com>
2026-01-06 11:02:54 -0600

    vbump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="shopify_sdk",
-    version="0.1.76",
+    version="0.1.77",
     author="Pk whiting",
     author_email="patten.whiting@gmail.com",
     description="An updated OO Sdk for Shopify GQL",
