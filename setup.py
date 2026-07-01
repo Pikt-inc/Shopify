545c33dc593836919420d6266c4b29bbf273aba9
Patten Whiting <patten.whiting@gmail.com>
2026-07-01 15:06:00 -0500

    Polish package metadata and license

--- setup.py
+++ setup.py
@@ -2,21 +2,45 @@
 from setuptools import setup, find_packages
 
 
+with open("README.md", encoding="utf-8") as readme_file:
+    long_description = readme_file.read()
+
+
 setup(
     name="shopify_sdk",
     version="0.1.88",
-    author="Pk whiting",
+    author="Patten Whiting",
     author_email="patten.whiting@gmail.com",
-    description="An updated OO Sdk for Shopify GQL",
+    description="Typed Python SDK for the Shopify Admin GraphQL API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/Pikt-inc/Shopify",
+    project_urls={
+        "Documentation": "https://github.com/Pikt-inc/Shopify#readme",
+        "Source": "https://github.com/Pikt-inc/Shopify",
+        "Issues": "https://github.com/Pikt-inc/Shopify/issues",
+    },
     packages=find_packages(),
+    license="MIT",
+    keywords=[
+        "shopify",
+        "graphql",
+        "sdk",
+        "pydantic",
+        "ecommerce",
+        "admin-api",
+    ],
     classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
     ],
     python_requires=">=3.10",
 )
